f1477ead946279eda9dfd758a2fe1f66bec637f9
Timur Shafigullin <Hometim55@gmail.com>
2018-03-10 23:24:55 +0300

    Migrate to heroku database

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,10 @@ app_settings = os.getenv(
     'app.config.BaseConfig'
 )
 app.config.from_object(app_settings)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:qwe123@localhost/soulcloud'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://fruyfjgqkbraaq' \
+                                        ':b4c3da4697d68c1e9deb92ad4a2df110fc9fefc7e39dedefd86258049c833110@ec2-50-16' \
+                                        '-217-122.compute-1.amazonaws.com:5432' \
+                                        '/ddduintd5jnpep?sslmode=require'
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
