1382b8f1a3c5b93ba55536f1ab22ec9ff84eeacd
Timur Shafigullin <Hometim55@gmail.com>
2018-03-17 23:30:00 +0300

    fix error handlers

--- app/error_handlers.py
+++ app/error_handlers.py
@@ -3,15 +3,15 @@ from app.models import Response
 
 
 @app.errorhandler(404)
-def method_not_found():
+def method_not_found(e):
     return Response("Method not found. Please, see documentation", False, 404).to_json(), 404
 
 
 @app.errorhandler(400)
-def bad_request():
+def bad_request(e):
     return Response("Bad request", False, 400).to_json(), 400
 
 
 @app.errorhandler(500)
-def internal_error():
+def internal_error(e):
     return Response("Houston, we have a problem. Please contact with API developers.", False, 500).to_json(), 500
